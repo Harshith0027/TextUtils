be6b9009b8952be8cfde0b7afbe30ebe252bee27
Harshith1124 <118748014+Harshith1124@users.noreply.github.com>
2024-09-18 15:38:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
+  const [mode,setMode] = useState('light');
+  const [alert,setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg : message,
+      tpyeof : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    },2000);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#052c65';
+      showAlert("Dark Mode has been enabled", "");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = '#f0f0f0';
+      showAlert("Light Mode has been enabled", "");
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router basename="/Textutils-React">
+        <Navbar title= 'TextUtils' modeForm={mode} changeMode={toggleMode}/>{/*navbar component*/}
+        <Alert makeAlert={alert} modeForm={mode}/>
+         
+          
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/Textutils-React"
+            element = {<TextForm label= 'Text Utility Tools | Word Convertor | Text Tool Area' viewAlert={showAlert} modeForm={mode}/> } />
+          </Routes>
+        </div>
+
+        <div className="container my-3">{/* changing of mode using button in a component container */}
+          <Routes>
+              <Route exact path="/about"
+              element = {<About title='About the app' modeForm={mode} /> } />
+          </Routes>
+        </div>
+    </Router>
+    </>
   );
 }
 
